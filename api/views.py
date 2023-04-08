c1697d0926ddf7ea5d902932b24fa589a42d39d0
Gualter Perinho <programadoracoriano@gmail.com>
2023-04-08 17:33:42 +0000

    token refresh

--- api/views.py
+++ api/views.py
@@ -1,6 +1,10 @@
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import generics
 from .serializers import *
+from rest_framework_simplejwt.views import TokenRefreshView
+
+class NewTokenRefreshView(TokenRefreshView):
+    pass
 
 class TokenObtainPairView(TokenObtainPairView):
     serializer_class = TokenObtainPairSerializer
