f90c08a313912a3b35368615b660f75030211131
Gualter Perinho <programadoracoriano@gmail.com>
2023-04-08 17:14:50 +0000

    token refresh

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import *
-
+from rest_framework_simplejwt.views import jwt_refresh_view
 urlpatterns = [
     path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('api/token/refresh/', jwt_refresh_view.as_view(), name='token_refresh'),
 ]
